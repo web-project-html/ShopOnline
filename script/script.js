cf9d4e0c6b0d357477c35176e4789f01f57ab8ff
Vinayrk2 <83126054+Vinayrk2@users.noreply.github.com>
2022-05-06 14:38:10 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,12 @@
+var click_rate = 0;
+
+function clicked(a){
+	if (click_rate%2 == 0) {
+	a.src = "https://cdn.iconscout.com/icon/free/png-256/close-3114451-2598229.png";
+	click_rate = click_rate + 1;
+}
+	else{
+	a.src = "https://flyclipart.com/thumb2/menu-three-horizontal-lines-symbol-868496.png";
+	click_rate = 0;
+	}
+}
